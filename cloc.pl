d1c696822d36d2bb0ea215c0f249c48c8e648d7d
Joel Rader <echophotographic@gmail.com>
2021-05-25 17:04:50 -0400

    Update cloc.pl
    added comments

--- cloc.pl
+++ cloc.pl
@@ -11,7 +11,7 @@
 
 	count_lines_of_code.pl - Perl script to count lines of code written in Perl or PHP
         The script counts lines of code, comments, blank lines and POD ( Plain Old Documentation with Perl Scripts )	
-	The script summarizes the lines of code for all files of Perl of PHP type, in a directory and sub directories
+	The script summarizes the lines of code for all files of Perl of PHP type in a directory and all sub directories
 	
 =head1 SYNOPSIS
 	
