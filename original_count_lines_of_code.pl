d0c601ce59686a5fdd8f0e4830ca3a01bdc785f9
Joel Rader <echophotographic@gmail.com>
2021-05-25 18:24:50 -0400

    Update original_count_lines_of_code.pl

--- original_count_lines_of_code.pl
+++ original_count_lines_of_code.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# File name: count_lines_of_code.pl
+# File name: original_count_lines_of_code.pl
 # Perl script to count lines of code written in Perl or PHP
 # Made for Developers
 # By Joel Rader  info[at]joelrader[dot]net
