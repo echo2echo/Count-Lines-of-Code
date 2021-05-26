b644d6cfcdc38adf9a32c8325c3f635c78e547c5
Joel Rader <echophotographic@gmail.com>
2021-05-26 18:42:41 -0400

    Update cloc.pl
    error handling for $filetype and usage

--- cloc.pl
+++ cloc.pl
@@ -38,7 +38,7 @@ my $filetype= "";
 
 $filetype = $ARGV[0];
 
-if (!$filetype) { usage(); }
+if (($filetype ne "--type=PHP") && ($filetype ne "--type=Perl")) { usage(); }
 
 my $sumTotal;
 my $sumBlock;
