3bf2f1b2c5966ffdaa227bf0bc52a0673488404f
Joel Rader <echophotographic@gmail.com>
2021-05-26 18:20:19 -0400

    Update cloc.pl
    altered usage description

--- cloc.pl
+++ cloc.pl
@@ -89,9 +89,9 @@ foreach $file ( @files ) {
 
 # Subroutine for script usage
 sub usage {
-	print "Script Usage: \$: ./cloc.pl --type=(PHP|Perl) \n";
-        print "Example: \$: ./cloc.pl --type=Perl\n";
-	print "Example \$: ./cloc.pl --type=PHP\n";
+	print "Script Usage: \$ ./cloc.pl --type=(PHP|Perl) \n";
+        print "Example: \$ ./cloc.pl --type=Perl\n";
+	print "Example \$ ./cloc.pl --type=PHP\n";
         exit;
 }
 
