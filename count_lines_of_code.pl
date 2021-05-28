24d30468b096122ff1181abb59218c51a84410d2
Joel Rader <echophotographic@gmail.com>
2021-05-28 01:29:15 -0400

    Add files via upload
    Added support for counting html lines of code

--- count_lines_of_code.pl
+++ count_lines_of_code.pl
@@ -1,27 +1,29 @@
 #!/usr/bin/env perl
 
-# File name: count_lines_of_code.pl
+# File name: cloc.pl
 # Perl script to count lines of code written in Perl or PHP
-# Made for busy Developers
 # By Joel Rader  info[at]joelrader[dot]net
 
 =pod
 
 =head1 NAME
 
-	count_lines_of_code.pl - Perl script to count lines of code written in Perl or PHP
+	count_lines_of_code.pl - Perl script to count lines of code written in Perl, PHP or HTML
         The script counts lines of code, comments, blank lines and POD ( Plain Old Documentation )	
-	The script summarizes the lines of code for all Perl or PHP files in a directory and all sub directories
+	The script summarizes the lines of code for all Perl, PHP or HTML files in a directory and all sub directories
 	
 =head1 SYNOPSIS
 	
-	$ ./count_lines_of_code.pl --type=(Perl||PHP)
+	$ ./count_lines_of_code.pl --type=(Perl||PHP||HTML)
 
 	# For example, to count all Perl files	
 	$ ./count_lines_of_code.pl --type=Perl
 	
 	# For example, to count all PHP files 
 	$ ./count_lines_of_code.pl --type=PHP
+		
+	# For example, to count all HTML files 
+	$ ./count_lines_of_code.pl --type=HTML
 	
 =head1 AUTHOR
 
@@ -40,7 +42,7 @@ $filetype = $ARGV[0];
 
 if (!$filetype) { usage(); }
 
-if (($filetype ne "--type=PHP") && ($filetype ne "--type=Perl")) { usage(); }
+if (($filetype ne "--type=PHP") && ($filetype ne "--type=Perl") && ($filetype ne "--type=HTML")) { usage(); }
 
 my $sumTotal;
 my $sumBlock;
@@ -50,6 +52,7 @@ my $sumLines;
 
 if ( $filetype =~ m/Perl/ ) { $command = "find . -name \"*.pl\" | sort"; }
 if ( $filetype =~ m/PHP/ ) { $command = "find . -name \"*.php\" | sort"; }
+if ( $filetype =~ m/HTML/ ) { $command = "find . -name \"*.html\" | sort"; }
 
 my @files = `$command`;
 
@@ -69,7 +72,7 @@ foreach $file ( @files ) {
   		usage();
 	}
 
-	if (($filetype ne "--type=PHP") && ($filetype ne "--type=Perl")) {
+	if (($filetype ne "--type=PHP") && ($filetype ne "--type=Perl") && ($filetype ne "--type=HTML"))  {
   		usage(); 
 	} 
 
@@ -80,8 +83,14 @@ foreach $file ( @files ) {
 	if ($filetype eq "--type=Perl") { 
   		perl_count(); 
 	}
+	
+	if ($filetype eq "--type=HTML") { 
+  		html_count(); 
+	}
+
 
 }
+
 
 
 
@@ -90,11 +99,19 @@ foreach $file ( @files ) {
 
 # Subroutine for script usage
 sub usage {
-	print "Script Usage: \$ ./count_lines_of_code.pl --type=(PHP|Perl)\n";
+	print "Script Usage: \$ ./count_lines_of_code.pl --type=(PHP|Perl|HTML) \n";
         print "Example: \$ ./count_lines_of_code.pl --type=Perl\n";
-	print "Example \$ ./count_lines_of_code.pl --type=PHP\n";
-        exit;
+	print "Example: \$ ./count_lines_of_code.pl --type=PHP\n";
+        print "Example: \$ ./count_lines_of_code.pl --type=HTML\n";
+	exit;
 }
+
+
+
+
+
+
+
 
 # Subroutine for Perl script line count
 sub perl_count {
@@ -152,6 +169,12 @@ print
  $sumLines += $lines;
 
 }
+
+
+
+
+
+
 
 # Subroutine for PHP script line count
 sub php_count {
@@ -216,6 +239,76 @@ print
 } 
 
 
+
+
+
+
+
+
+
+
+# Subroutine for HTML file line count
+sub html_count {
+	if ($file !~ m/(\.html)/) {
+		print "Script --type flag was set for counting HTML file lines of code.\nThe file should end in .html\n";
+		exit;
+	}
+
+	open FILE, $file or die "Could not open file! $!";
+
+	my $count = my $slurp_lines = my $total = my $blank = my $number_of_comment_lines = my $comment = 0;
+	my @comment_lines;
+	my $comment_number;
+	my @count;
+
+
+	while (<FILE>) {
+		
+		$total++;
+		$slurp_lines .= $_;	
+		
+		if ($_ =~ m/^\s*$/){ $blank++; next; }
+		$count++;
+
+	}	
+	
+	while ( $slurp_lines =~ /(<!--(?!-?>).*?--\s*>)/sg ) {
+
+    		$comment = $1;
+
+    		@comment_lines = split /\n/, $comment;
+
+    		$number_of_comment_lines += @comment_lines;
+			
+	}
+
+	  my $lines = $count - $number_of_comment_lines;
+
+	close(FILE);
+	
+	$file =~ s/\.\///;
+
+	print "Lines of Code for $file";
+
+print 
+" Total:    $total
+ Comments: $number_of_comment_lines
+ Blank:    $blank
+ Code:     $lines\n";
+
+
+
+ $sumTotal += $total;
+ $sumComments += $number_of_comment_lines;
+ $sumBlank += $blank;
+ $sumLines += $lines;
+
+
+
+	
+} 
+
+
 if ( $filetype eq "--type=PHP") {
 	print "~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
 	print "Summary for all PHP files: \n";
@@ -243,5 +336,15 @@ if ( $filetype eq "--type=Perl") {
 }
 
 
+if ( $filetype eq "--type=HTML") {
+	print "~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+	print "Summary for all HTML files: \n";
+	print "~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+	print 
+ " Total: $sumTotal
+ Comments: $sumComments
+ Blank: $sumBlank
+ Code: $sumLines\n";
 
+}
 
